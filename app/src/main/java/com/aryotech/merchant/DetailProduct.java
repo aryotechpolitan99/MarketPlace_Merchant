56d09ca4b3fa62112301f8149f76add2abfdc208
aryotech <aryofreeyoung@gmail.com>
2020-03-11 16:09:54 +0700

    deleted product yang ada di merchant

--- app/src/main/java/com/aryotech/merchant/DetailProduct.java
+++ app/src/main/java/com/aryotech/merchant/DetailProduct.java
@@ -10,15 +10,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.aryotech.merchant.Model.AccessToken;
 import com.aryotech.merchant.Model.Product;
 import com.aryotech.merchant.Model.Update;
+import com.aryotech.merchant.Utils.TokenManager;
 import com.bumptech.glide.Glide;
+
+import java.util.Hashtable;
+import java.util.Map;
 
 public class DetailProduct extends AppCompatActivity {
 
@@ -28,6 +34,7 @@ public class DetailProduct extends AppCompatActivity {
     public static String detailprod = "";
     private RequestQueue requestQueue;
 
+    AccessToken accessToken;
     long id;
 
     @Override
@@ -35,9 +42,12 @@ public class DetailProduct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_product);
 
+        accessToken = TokenManager.getInstance(getSharedPreferences("pref",MODE_PRIVATE)).getToken();
         requestQueue = Volley.newRequestQueue(this);
         Product product = getIntent().getParcelableExtra(detailprod);
         inisialisasi();
+
+        id = product.getProductId();
 
         btnUpdate = findViewById(R.id.btn_update);
         btnUpdate.setOnClickListener(new View.OnClickListener() {
@@ -55,7 +65,7 @@ public class DetailProduct extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 final StringRequest stringRequest = new StringRequest(Request.Method.DELETE,
-                        "http://210.210.154.65:4444/api/product/" + id + "/delete", new Response.Listener<String>() {
+                        "http://210.210.154.65:4444/api/merchant/product/" + id + "/delete", new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Toast.makeText(DetailProduct.this, "Success Deleted", Toast.LENGTH_SHORT).show();
@@ -63,9 +73,22 @@ public class DetailProduct extends AppCompatActivity {
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        Toast.makeText(DetailProduct.this,"gagal delete", Toast.LENGTH_SHORT).show();
                     }
-                });
+                })
+                {
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String,String> headers = new Hashtable<>();
+
+                        headers.put("Accept","application/json");
+                        headers.put("Content-Type","application/x-www-form-urlencoded");
+                        // Autirization bearer token
+                        headers.put("Authorization",accessToken.getTokenType()+" "+accessToken.getAccessToken());
+                        return headers;
+                    }
+                };
 
                 requestQueue.add(stringRequest);
             }
